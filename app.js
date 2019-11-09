b50b7a48392adc5d411f44ed70540a5ecb91162a
Simon Stanford <simonjstanford@gmail.com>
2019-11-09 16:36:37 +0000

    Added the port environment variable

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.post("/compose", function(req, res) {
   console.log(req.body.postTitle);
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port " + process.env.PORT || 3000);
+})
